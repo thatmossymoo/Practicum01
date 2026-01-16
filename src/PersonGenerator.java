61f33697097d165cec9f00788d69bce133b3d23e
thatmossymoo <grace.lafountain3@gmail.com>
2026-01-16 17:16:22 -0500

    second video end

--- src/PersonGenerator.java
+++ src/PersonGenerator.java
@@ -1,5 +1,11 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Scanner;
+import static java.nio.file.StandardOpenOption.CREATE;
 
 void main()
 /*
@@ -10,8 +16,12 @@ Title (a string like Mr., Mrs., Ms., Dr., etc.)
 YearOfBirth (an int)
  */
 {
+    File workingDirectory = new File(System.getProperty("user.dir"));
+    Path file = Paths.get(workingDirectory.getPath() + "\\src\\PersonTestData.txt");
+
     ArrayList<String> folks = new ArrayList<>();
     Scanner in = new Scanner(System.in);
+
     boolean done = false;
     String personRec = "";
     String ID = "";
@@ -35,4 +45,31 @@ YearOfBirth (an int)
 
     for(String p: folks)
         System.out.println(p);
+
+    try
+    {
+        // Typical java pattern of inherited classes
+        // we wrap a BufferedWriter around a lower level BufferedOutputStream
+        OutputStream out =
+                new BufferedOutputStream(Files.newOutputStream(file, CREATE));
+        BufferedWriter writer =
+                new BufferedWriter(new OutputStreamWriter(out));
+
+        // Finally can write the file LOL!
+
+        for(String rec : folks)
+        {
+            writer.write(rec, 0, rec.length());  // stupid syntax for write rec
+            // 0 is where to start (1st char) the write
+            // rec. length() is how many chars to write (all)
+            writer.newLine();  // adds the new line
+
+        }
+        writer.close(); // must close the file to seal it and flush buffer
+        System.out.println("Data file written!");
+    }
+    catch (IOException e)
+    {
+        e.printStackTrace();
+    }
 }
